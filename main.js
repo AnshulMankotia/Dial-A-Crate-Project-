eb82a7da29d1da5c799ff53db860391bbeb166a0
Nikhil Dhiman <dhimannikhil905@gmail.com>
2023-03-07 17:39:57 +0530

    added-age-verify-Dropdown and Login-page-Slider

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
+const toClick = document.querySelector('.LogIn')
+const toBlock = document.querySelector('.log__overlay')
+const toActive = document.querySelector('.login-page')
+const clickYes = document.querySelector('.yes')
+const clickNo = document.querySelector('.no')
+const addhidden = document.querySelector('#age-verify')
+const towindow = document.querySelector('.window')
+window.addEventListener("load", () => {
+    towindow.classList.add("active")
+});
+clickYes.addEventListener('click', () => {
+    addhidden.classList.add("hidden")
+});
+clickNo.addEventListener('click', () => {
+    addhidden.classList.add("")
+});
+toClick.addEventListener('click', () => {
+    toBlock.style.display = "block";
+    toActive.classList.add("open");
+});
+toBlock.addEventListener('click', () => {
+    toBlock.style.display = "none";
+    toActive.classList.remove("open");
+});
+
+
 const add=document.querySelector('.add')
 const sub=document.querySelector('.sub')
 const display=document.querySelector('.display')
-
 add.addEventListener('click', () =>{
 if(display.value>=0){
     display.value++;
@@ -16,5 +41,6 @@ else{
     display.value=parseInt(display.value-1);
 }
 });
+
 
 
