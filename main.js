503e0de2ffcf2b33cf9569d6d7b7394986f70c29
Anshul Mankotia <anshulmankotia1997@gmail.com>
2023-03-07 12:35:53 +0530

    merge code with akhil

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+
+// ---------------SEARCH DROPDOWN---------------------
+    const searchBar =document.querySelector(".searchDropdown");
+    const searchIcon = document.querySelector(".searchIcon");
+    searchIcon.addEventListener("click",()=>{
+        searchBar.classList.add("active");
+    });
+    
+    const overlay = document.querySelector(".overlay");
+    overlay.addEventListener("click",()=>{
+        searchBar.classList.remove("active")
+    });
+
+// ---------------SEARCH DROPDOWN---------------------
 const add=document.querySelector('.add')
 const sub=document.querySelector('.sub')
 const display=document.querySelector('.display')
@@ -16,5 +30,6 @@ else{
     display.value=parseInt(display.value-1);
 }
 });
+
 
 
