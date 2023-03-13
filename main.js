aab036d7a800b266a8e1037abaec3bbb4920cff1
Nikhil Dhiman <dhimannikhil905@gmail.com>
2023-03-13 10:19:01 +0530

    working on mobile-design

--- main.js
+++ main.js
@@ -4,6 +4,22 @@
 const toClick = document.querySelector('.LogIn')
 const toBlock = document.querySelector('.log__overlay')
 const toActive = document.querySelector('.login-page')
+const menu = document.querySelector(".menu_icon")
+const tocloseMenu = document.querySelector(".fa-circle-xmark")
+const navSlider = document.querySelector(".tab_nav-container")
+const toclosSpirits = document.querySelector("#closespirits")
+const clicktosubnav = document.querySelector(".tab_spirits-rel")
+const subNav = document.querySelector(".tab_spirits-dropdown")
+const scotch = document.querySelector(".arrow_scotch")
+const scotchnav = document.querySelector(".tab_sub-drop")
+const tequila = document.querySelector(".arrow_tequila")
+const tequilaNAv = document.querySelector(".tequila-sub-drop") 
+const whiskey = document.querySelector(".arrow_whiskey")
+const whiskeyNAv = document.querySelector(".whiskey-sub-drop") 
+const closeScotch = document.querySelector("#closescotch")
+const closeSTequila = document.querySelector("#closetequila")
+const closeWhiskey = document.querySelector("#closewhiskey")
+
 
 toClick.addEventListener('click', () => {
     toBlock.style.display = "block";
@@ -13,7 +29,43 @@ toClick.addEventListener('click', () => {
 toBlock.addEventListener('click', () => {
     toActive.classList.remove("open");
     toBlock.style.display = "none";
+    navSlider.classList.remove("active")
+    subNav.classList.remove("active")
+    scotchnav.classList.remove("active") 
+    tequilaNAv.classList.remove("active")
 });
+menu.addEventListener('click',()=>{
+    navSlider.classList.add("active")
+    toBlock.style.display = "block";
+});
+tocloseMenu.addEventListener('click',()=>{
+    navSlider.classList.remove("active")
+    toBlock.style.display = "none";
+})
+clicktosubnav.addEventListener('click',()=>{
+    subNav.classList.add("active")
+});
+toclosSpirits.addEventListener('click',()=>{
+    subNav.classList.remove("active")
+})
+scotch.addEventListener('click',()=>{
+    scotchnav.classList.add("active") 
+});
+tequila.addEventListener('click',()=>{
+    tequilaNAv.classList.add("active")
+});
+whiskey.addEventListener('click',()=>{
+    whiskeyNAv.classList.add("active")
+});
+closeScotch.addEventListener('click',()=>{
+    scotchnav.classList.remove("active") 
+});
+closeSTequila.addEventListener('click',()=>{
+    tequilaNAv.classList.remove("active")
+})
+closeWhiskey.addEventListener('click',()=>{
+    whiskeyNAv.classList.remove("active")
+})
 
 
 
@@ -68,6 +120,15 @@ sub.addEventListener('click', () => {
         display.value = parseInt(display.value - 1);
     }
 });
+
+// ACCORDION
+const accordion = document.getElementsByClassName('accordion');
+
+for (i=0; i<accordion.length; i++) {
+  accordion[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+  })
+}
 
 
 
